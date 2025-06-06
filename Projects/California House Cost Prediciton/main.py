783af48819ef543843186f9d84fe3679301066c9
Artem <122204149+letv1nnn@users.noreply.github.com>
2025-06-06 12:16:45 +0100

    Update main.py

--- Projects/California House Cost Prediciton/main.py
+++ Projects/California House Cost Prediciton/main.py
@@ -5,7 +5,7 @@ import urllib.request
 import matplotlib.pyplot as plt
 import numpy as np
 from zlib import crc32
-from sklearn.model_selection import train_test_split, StratifiedShuffleSplit
+from sklearn.model_selection import train_test_split
 
 def load_housing_data():
     tarball_path = Path("datasets/housing.tgz")
@@ -25,13 +25,15 @@ housing = load_housing_data()
 # print(housing.describe())
 
 # DATA VISUALIZATION
-def visualize():
+def visualize_initial_dataset():
     housing.hist(bins=50, figsize=(12, 8))
     plt.show()
 
+# visualize_initial_dataset()
+
 # SPLITTING DATA INTO TRAIN AND TEST SETS
 train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
-# 1 method (not efficient)
+# manual implementation
 def shuffle_and_split_data(data, test_ration):
     shuffled_indices = np.random.permutation(len(data))
     test_set_size = int(len(data) * test_ration)
@@ -39,31 +41,44 @@ def shuffle_and_split_data(data, test_ration):
     train_indices = shuffled_indices[test_set_size:]
     return data.iloc[train_indices], data.iloc[test_indices]
 
-# 2 method
-def is_id_in_test_set(identifier, test_ratio):
-    return crc32(np.int64(identifier)) < test_ratio * 2 ** 32
 
-def split_data_with_id_hash(data, test_ratio, id_column):
-    ids = data[id_column]
-    in_test_set = ids.apply(lambda id_: is_id_in_test_set(id_, test_ratio))
-    return data.loc[~in_test_set], data.loc[in_test_set]
-
-# 3 method
+# splitting the median house district into 6 categories
 housing["income_cat"] = pd.cut(housing["median_income"],
                                bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
                                labels=[1, 2, 3, 4, 5])
-housing["income_cat"].value_counts().sort_index().plot.bar(rot=0, grid=True)
+def visualize_income_cat():
+    housing["income_cat"].value_counts().sort_index().plot.bar(rot=0, grid=True)
+    plt.xlabel("Income category")
+    plt.ylabel("Number of districts")
+    plt.show()
 
-splitter = StratifiedShuffleSplit(n_splits=10, test_size=0.2, random_state=42)
-strat_splits = []
-for train_index, test_index in splitter.split(housing, housing["income_cat"]):
-    strat_train_set_n = housing.iloc[train_index]
-    strat_test_set_n = housing.iloc[test_index]
-    strat_splits.append([strat_train_set_n, strat_test_set_n])
+# visualize_income_cat()
 
-strat_train_set, strat_test_set = strat_splits[0]
+# Ensures all important classes are represented in both sets
 strat_train_set, strat_test_set = train_test_split(
     housing, test_size=0.2, stratify=housing["income_cat"], random_state=42)
-strat_test_set["income_cat"].value_counts() / len(strat_test_set)
+
+# print(strat_test_set.info())
+# Income category proportion int the test set
+# print(strat_test_set["income_cat"].value_counts() / len(strat_test_set))
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop("income_cat", axis=1, inplace=True)
+
+
+# copy of the original training set, so I could do various transformations with it without change the initial one
+housing = strat_train_set.copy()
+
+# The area of each circle represents the district's population
+# and the color represents the price of the house
+def visualize_long_lat():
+    housing.plot(kind="scatter", x="longitude", y="latitude", grid=True, alpha=0.2,
+                 s=housing["population"] / 100, label="population",
+                 c="median_house_value", cmap="jet", colorbar=True,
+                 legend=True, sharex=False, figsize=(10, 7)
+                 )
+    plt.show()
+
+# visualize_long_lat()
+
 
 
